807e16e223fb0260507f4ba7f8bd75e13acddd51
Ha Jihyun <kimwatson2026@gmail.com>
2024-11-25 14:01:52 +0900

    feat. 게시글, 댓글에서 작성사 프로필 사진 노출

--- src/main/java/com/jungle/studybbitback/domain/room/dto/roomboard/GetRoomBoardResponseDto.java
+++ src/main/java/com/jungle/studybbitback/domain/room/dto/roomboard/GetRoomBoardResponseDto.java
@@ -1,5 +1,6 @@
 package com.jungle.studybbitback.domain.room.dto.roomboard;
 
+import com.jungle.studybbitback.domain.member.entity.Member;
 import com.jungle.studybbitback.domain.room.entity.roomboard.RoomBoard;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,16 +16,18 @@ public class GetRoomBoardResponseDto {
     private String title;
     private String content;
     private String createdBy;
+    private String profileImageUrl;
     private LocalDateTime createdAt;
     private Long roomId;
 
     // RoomBoard 엔티티를 DTO로 변환
-    public static GetRoomBoardResponseDto from(RoomBoard roomBoard, String createdByNickname) {
+    public static GetRoomBoardResponseDto from(RoomBoard roomBoard, Member member) {
         return new GetRoomBoardResponseDto(
                 roomBoard.getId(),
                 roomBoard.getTitle(),
                 roomBoard.getContent(),
-                createdByNickname, // 닉네임을 외부에서 전달
+                member.getNickname(), // 닉네임을 외부에서 전달
+                member.getProfileImageUrl(),
                 roomBoard.getCreatedAt(),
                 roomBoard.getRoom().getId()
         );
