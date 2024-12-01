3948ab38093625379c51fe350206b10fbd69d1c9
Hyobeom Jang <gyqjajang@gmail.com>
2024-12-01 15:18:03 +0900

    fix : 회원조회 시 오늘자 순공시간 같이 조회

--- src/main/java/com/jungle/studybbitback/domain/member/dto/FindMemberResponseDto.java
+++ src/main/java/com/jungle/studybbitback/domain/member/dto/FindMemberResponseDto.java
@@ -1,7 +1,10 @@
 package com.jungle.studybbitback.domain.member.dto;
 
+import com.jungle.studybbitback.domain.dailystudy.entity.DailyStudy;
 import com.jungle.studybbitback.domain.member.entity.Member;
 import lombok.Getter;
+
+import java.time.Duration;
 
 @Getter
 public class FindMemberResponseDto {
@@ -10,12 +13,18 @@ public class FindMemberResponseDto {
 	private String nickname;
 	private String profileImageUrl;
 	private Double flowTemperature;
+	private Duration studyTime;
 
-	public FindMemberResponseDto(Member member) {
+	public FindMemberResponseDto(Member member, DailyStudy dailyStudy) {
 		this.id = member.getId();
 		this.email = member.getEmail();
 		this.nickname = member.getNickname();
 		this.profileImageUrl = member.getProfileImageUrl();
 		this.flowTemperature = member.getFlowTemperature();
+		if(dailyStudy == null) {
+			this.studyTime = Duration.ofHours(0);
+		} else {
+			this.studyTime = dailyStudy.getStudyTime();
+		}
 	}
 }
