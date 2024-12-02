1eb3a6c9159e69e3e336fc175e76c28c3fc34961
Hyobeom Jang <gyqjajang@gmail.com>
2024-12-02 13:42:49 +0900

    fix : 안쓰는 코드 삭제

--- src/main/java/com/jungle/studybbitback/domain/posting/dto/comment/AddCommentRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.jungle.studybbitback.domain.posting.dto.comment;
-
-import lombok.Getter;
-
-@Getter
-public class AddCommentRequestDto {
-    private String content;
-}
--- src/main/java/com/jungle/studybbitback/domain/posting/dto/posting/DeletePostingRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.jungle.studybbitback.domain.posting.dto.posting;
-
-import lombok.Getter;
-
-@Getter
-public class DeletePostingRequestDto {
-    private String password;
-}
